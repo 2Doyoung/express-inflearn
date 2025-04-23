2ab70416e2d1ae12a3697b0e42b0a0eb89209806
2Doyoung <dlehdud1653@kakao.com>
2025-04-23 18:21:31 +0900

    middleware2

--- myapp/app.js
+++ myapp/app.js
@@ -6,12 +6,23 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.use((req, res, next) => {
-    console.log('모든 코드에서 실행');
+    console.log('모든 코드1에서 실행');
+    next();
+}, (req, res, next) => {
+    console.log('모든 코드2에서 실행');
+    next();
+}, (req, res, next) => {
+    console.log('모든 코드3에서 실행');
     next();
 })
+// }, (req, res, next) => {
+//     throw new Error('Error');
+// })
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+    res.send('한 라우터에 응답(res)을 두 번 이상 보내려고 할 때');
+    res.json({ hello : 'LEE' })
 });
 
 app.get('/about', (req, res) => {
@@ -45,6 +56,15 @@ app.post('/', (req, res) => {
 // app.get('/*', (req, res) => {
 //     res.send('Hello Every')
 // })
+
+app.use((req, res, next) => {
+    res.send('404 에러')
+})
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.send('에러페이지');
+})
 
 app.listen(app.get('port'), () => {
     console.log('Express Server Start');
